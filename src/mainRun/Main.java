9a96a360b4da758a8927c72517db163d9e82a2f0
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2019-07-15 12:26:01 -0500

    marge.

--- src/mainRun/Main.java
+++ src/mainRun/Main.java
@@ -1,5 +1,8 @@
 package mainRun;
 
+import factory.FactoryCar;
+import factory.FactoryDao;
+import factory.FactoryPeople;
 import gui.MainFrame;
 import gui.content.people.PeopleContainerMainGui;
 
@@ -7,6 +10,19 @@ public class Main {
 
 	public static void main(String[] args) {
 
+
+		FactoryPeople factoryPeople = new FactoryPeople();
+		FactoryCar factoryCar = new FactoryCar();
+		try {
+			FactoryDao.crearFabricaDao(factoryPeople);
+			FactoryDao.crearFabricaDao(factoryCar);
+			
+			
+		}catch (Exception e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+		
 			new MainFrame(new PeopleContainerMainGui());
+			
 	}
 }
