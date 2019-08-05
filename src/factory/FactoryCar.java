1e7803aa096e92139ade925428c0ec1bba51d776
Amanda Franco <amanda.vianey.franco.uribe.306@gmail.com>
2019-08-05 09:17:58 -0500

    comentarios

--- src/factory/FactoryCar.java
+++ src/factory/FactoryCar.java
@@ -3,12 +3,25 @@ package factory;
 import dao.DaoCar;
 import dao.DaoInterface;
 
-public class FactoryCar implements ObjectDao{
+/**
+ * Archivo: FactoryCar.java contiene la definición de la clase FactoryCar que
+ * implementa ObjectDao
+ * 
+ * @author Jorge Jacobo, Marcos Moreno, Gabriel Garcia, Amanda Franco
+ * @version 1.0
+ *
+ */
+public class FactoryCar implements ObjectDao {
 
+	/**
+	 * Método crearDao
+	 * 
+	 * @return retorna un objeto.
+	 */
 	@Override
 	public DaoInterface<?> crearDao() {
 		DaoCar daoCar = new DaoCar();
 		return daoCar;
-	}
+	}// cierre método crearDao
 
-}
+}// cierre clase FactoryCar
